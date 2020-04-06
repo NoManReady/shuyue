4eab314349ab71d411f0cb75bbf6f074b662470e
强堃 <453421041@qq.com>
2020-04-06 16:57:46 +0800

    修改路由路径

--- src/router/index.js
+++ src/router/index.js
@@ -5,20 +5,21 @@ const Home = () => import("@/views/Home");
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  }
-];
 let base = "/";
 if (window.__POWERED_BY_QIANKUN__) {
   base = "/app/shuyue";
 }
+const routes = [
+  {
+    path: base,
+    name: "Home",
+    component: Home
+  }
+];
 
 const router = new VueRouter({
   mode: "hash",
+  fallback:true,
   base,
   routes
 });
